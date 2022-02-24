1f5ec7a1d03925f517ddb4525a6c16853ce5cca3
MukhammadUmar <MukhammadumarMukhammadzoda@gmail.com>
2022-02-24 09:44:39 +0500

    Updated

--- face.py
+++ face.py
@@ -4,9 +4,11 @@ image = cv2.imread('Images/img3.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 face_detector = cv2.CascadeClassifier('cascade/data/haarcascade_frontalface_alt.xml')
-detection = face_detector.detectMultiScale(image, scaleFactor=1.1 )
+detection = face_detector.detectMultiScale(image, scaleFactor=1.1,
+                                           minNeighbors=1, maxSize=(60,60))
 
 for x,y,w,h in detection:
     cv2.rectangle(image, (x,y), (x+w, y+h), (0,0,200), 2)
+    print(w,h)
 cv2.imshow('', image)
 cv2.waitKey(0)
